9b7c449803a486b20d6a8ea47d8f9f641cbbbd2c
Michelle Sánchez <A01376622@itesm.mx>
2022-09-07 16:52:18 -0500

    Commit inicial
    Pagina web bio

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 });
 
-app.get('/about-me', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
-});
-
 //Lanzar la app
 app.listen(PORT,()=>{
     console.log("servidor en línea");
